f7b7d3a24223b006b320e7273e8f8cb60a1f6290
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-12-26 08:09:28 +0400

    fix(45): removed unnecessary code

--- src/pages/user/Home/Home.tsx
+++ src/pages/user/Home/Home.tsx
@@ -1,45 +1,7 @@
-import { useState } from 'react';
-import { useAppDispatch } from 'store/index';
-import { Select, Option, Modal } from 'components';
-import { addFolder } from 'store/folder';
-
 const Home = () => {
-  const dispatch = useAppDispatch();
-
-  const [selectedOption, setSelectedOption] = useState<string | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
-
-  const handleChange = (option: string) => {
-    setSelectedOption(option);
-  };
-
-  const handleRequest = () => {
-    dispatch(
-      addFolder({
-        description: 'descriptionnery',
-        name: 'namery',
-      })
-    );
-  };
-
   return (
     <div>
-      <h1 style={{ fontSize: '40px', marginBottom: '24px' }}>{selectedOption}</h1>
-      <Select selectedOption={selectedOption} onChange={handleChange}>
-        <Option value="Universesssss">
-          <div>I love you&nbsp;</div>
-          <span style={{ color: 'mediumpurple' }} className="Hasmikkssss">
-            My Universe
-          </span>
-        </Option>
-        <Option value="Option2">Option22</Option>
-        <Option value="Option3">Option33</Option>
-      </Select>
-      <hr />
-      {/* <Modal isOpen={isModalOpen} title="Add Command" onClose={() => setIsModalOpen(false)}>
-        Modalnery
-      </Modal> */}
-      <button onClick={handleRequest}>add folder</button>
+      <h1 style={{ fontSize: '40px', marginBottom: '24px' }}>Home</h1>
     </div>
   );
 };
