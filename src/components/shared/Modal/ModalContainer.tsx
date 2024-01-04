ff056f9178e1220aa505581f2213dd9cdad3c94f
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-01-04 08:37:52 +0400

    feat(42): made body overflow hidden while modal is open

--- src/components/shared/Modal/ModalContainer.tsx
+++ src/components/shared/Modal/ModalContainer.tsx
@@ -2,9 +2,13 @@ import { FC } from 'react';
 
 import Modal from './Modal';
 
+import useModalContainer from './useModalContainer';
+
 import { IModalContainerProps } from './types';
 
 const ModalContainer: FC<IModalContainerProps> = ({ title, isOpen, onClose, children }) => {
+  useModalContainer(isOpen);
+
   return isOpen ? (
     <Modal isOpen={isOpen} title={title} onClose={onClose}>
       {children}
