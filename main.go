a87c4ae36d7d55886dc4d5647f61c7df7f750d58
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-08-05 23:54:52 +0400

    initialized fyne aapp

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+type App struct {
+	App        fyne.App
+	MainWindow fyne.Window
+}
 
 func main() {
-	fmt.Println("Hello world")
+	var myApp App
+
+	myApp.App = app.NewWithID("com.keepcommand.aa")
+	myApp.MainWindow = myApp.App.NewWindow("Keep Command")
+	myApp.MainWindow.Resize(fyne.NewSize(800, 600))
+
+	myApp.MainWindow.ShowAndRun()
 }
