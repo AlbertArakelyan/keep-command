938a4d84b96193e38182348b5eb2bfaece03f811
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-08-09 23:55:25 +0400

    feat(1): created main layout

---
+++ layout/main-layout.go
@@ -0,0 +1,12 @@
+package layout
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+)
+
+func NewMainLayout(mainWindow fyne.Window) func(page *fyne.Container) *fyne.Container {
+	return func(page *fyne.Container) *fyne.Container {
+		return container.NewBorder(nil, nil, nil, nil, page)
+	}
+}
