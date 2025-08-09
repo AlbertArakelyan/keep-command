861f57d06a656040dfa888d33d810c77f8c9eb4c
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-08-09 23:44:19 +0400

    added tags for folders and commands

--- models/folder.go
+++ models/folder.go
@@ -6,6 +6,7 @@ type Folder struct {
 	ID          int    `gorm:"primary_key;auto_increment"`
 	Name        string `gorm:not null;unique"`
 	Description string
+	FolderTags  string    `gorm:"not null"`
 	CreatedAt   time.Time `gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `gorm:"autoUpdateTime"`
 }
