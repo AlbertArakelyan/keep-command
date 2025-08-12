9e445c4a16b3797beb14a12971f66986c680cc18
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-08-12 23:20:22 +0400

    feat(5): moved sqlite db to a separate package

--- db/db.go
+++ db/db.go
@@ -1,23 +1,22 @@
 package db
 
 import (
+	sqlitedb "github.com/AlbertArakelyan/keep-command/db/sqlite-db"
 	"github.com/AlbertArakelyan/keep-command/models"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
-
 func InitDB() {
 	var err error
 
-	DB, err = gorm.Open(sqlite.Open("sql.db"), &gorm.Config{})
+	sqlitedb.DB, err = gorm.Open(sqlite.Open("sql.db"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
 	// Automigrate models
-	err = DB.AutoMigrate(&models.Folder{}, &models.Command{})
+	err = sqlitedb.DB.AutoMigrate(&models.Folder{}, &models.Command{})
 	if err != nil {
 		panic(err)
 	}
