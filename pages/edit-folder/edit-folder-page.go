49de19056d712f00df647ea33f1a467512e98f2a
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-08-17 22:06:13 +0400

    feat(22): created edit page form

--- pages/edit-folder/edit-folder-page.go
+++ pages/edit-folder/edit-folder-page.go
@@ -1,14 +1,65 @@
 package editfolder
 
 import (
+	"fmt"
+	"image/color"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"github.com/AlbertArakelyan/keep-command/constants"
 	"github.com/AlbertArakelyan/keep-command/state"
 )
 
 func EditFolderPage() *fyne.Container {
-	return container.NewVBox(
-		widget.NewLabel(state.EditingFolder.Name),
+	editBarTitle := canvas.NewText(fmt.Sprintf("✏️ Editing \"%s\" Folder", state.EditingFolder.Name), nil)
+	editBarTitle.Alignment = fyne.TextAlignCenter
+	editBarTitle.TextSize = constants.PageTitleFontSize
+	editBarTitle.TextStyle = fyne.TextStyle{Bold: true}
+
+	backButton := widget.NewButton("⬅️ Back", func() {
+		state.EditingFolder = nil
+		state.MyApp.SetActiveContent(state.MyApp.CommandsPage())
+	})
+
+	editBar := container.NewHBox(
+		editBarTitle,
+		layout.NewSpacer(),
+		backButton,
 	)
+
+	titleEntry := widget.NewEntry()
+	titleEntry.SetPlaceHolder("Folder Title *")
+	titleEntry.SetText(state.EditingFolder.Name)
+
+	descriptionEntry := widget.NewMultiLineEntry()
+	descriptionEntry.SetPlaceHolder("Folder Description")
+	descriptionEntry.SetText(state.EditingFolder.Description)
+
+	tasgEntry := widget.NewEntry()
+	tasgEntry.SetPlaceHolder("Tags *")
+	tasgEntry.SetText(state.EditingFolder.FolderTags)
+
+	editFolderButton := widget.NewButton("💾 Edit Folder", func() {})
+	// editFolderButton.Importance = widget.HighImportance
+
+	formContainer := container.NewVBox(
+		titleEntry,
+		descriptionEntry,
+		container.NewVBox(
+			tasgEntry,
+			widget.NewLabel("*Separate by commas, e.g. go,docker,javascript"),
+		),
+		editFolderButton,
+	)
+
+	editFolderPageContent := container.NewVBox(
+		editBar,
+		canvas.NewLine(color.White),
+		formContainer,
+	)
+
+	return editFolderPageContent
 }
